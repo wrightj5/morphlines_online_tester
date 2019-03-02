5abf281fdb2822e827daee6fbe9ac8960b1c3979
Cathy Guarino <cathy.l.guarino@gmail.com>
2019-03-03 01:12:59 +1100

    Updated styles to use tailwind + added working tabs

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ npm install --save body-parser
 npm install --save child_process
 npm install --save ejs
 npm install --save tmp
+npm install --save tailwindcss
--- public/js/syntax.js
+++ public/js/syntax.js
@@ -1,32 +1,41 @@
 window.onload = function () {
     var codeMirror = CodeMirror.fromTextArea(document.getElementById('config'), {
         mode: "yaml",
-        theme: "monokai",
+        theme: "nord",
         lineNumbers: true,
         lineWrapping: true,
-    });
+        styleActiveLine: true,
+        styleActiveSelected: true,
+    }).setSize("100%", "100%");
     var codeMirror = CodeMirror.fromTextArea(document.getElementById('data'), {
         mode: "javascript",
-        theme: "monokai",
+        theme: "nord",
         lineNumbers: true,
         lineWrapping: true,
-    });
+        styleActiveLine: true,
+        styleActiveSelected: true,
+    }).setSize("100%", "100%");
     var codeMirror = CodeMirror.fromTextArea(document.getElementById('result'), {
         mode: "javascript",
-        theme: "monokai",
+        theme: "nord",
         lineNumbers: true,
         lineWrapping: true,
         extraKeys: {"Ctrl-Q": function(cm) { cm.foldCode(cm.getCursor()); }},
         foldGutter: true,
         gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-    });
+        styleActiveLine: false,
+        styleActiveSelected: false,
+    }).setSize("100%", "100%");
+
+    document.getElementById("defaultTab").click();
+
 }
 $(document).on('keyup keydown change', '.editor', function(){
   var myCodeMirror = CodeMirror(function(elt) {
       $('.editor').replaceWith(elt);
   }, {value: $('.editor').val(),
       lineNumbers: true,
-      theme: "monokai",
+      theme: "nord",
       lineWrapping: true,
       mode: "yaml"});
 });
@@ -35,7 +44,7 @@ $(document).on('keyup keydown change', '.editor2', function(){
       $('.editor2').replaceWith(elt);
   }, {value: $('.editor2').val(),
       lineNumbers: true,
-      theme: "monokai",
+      theme: "nord",
       lineWrapping: true,
       mode: "javascript"});
 });
@@ -45,6 +54,20 @@ $(document).on('keyup keydown change', '.result', function(){
   }, {value: $('.result').val(),
       lineNumbers: true,
       lineWrapping: true,
-      theme: "monokai",
+      theme: "nord",
       mode: "javascript"});
 });
+
+function tabResult(evt, result) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" tabactive", "");
+  }
+  document.getElementById(result).style.display = "block";
+  evt.currentTarget.className += " tabactive";
+}
